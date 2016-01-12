1085540f451e6b29ff6402a8d95d090f17ad8812
Afdal Afif <Afdal Afif>
2016-01-12 17:58:08 +0700

    login CBS versi 8

---
+++ login-cbs/src/main/java/com/corebanking/login/LoginCbsApplication.java
@@ -0,0 +1,12 @@
+package com.corebanking.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginCbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginCbsApplication.class, args);
+	}
+}
